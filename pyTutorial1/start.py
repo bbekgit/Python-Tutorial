29f5fcd7d78f2c1d57dd9530626fa77747000940
Bibek Gupta <35547667+bbekgit@users.noreply.github.com>
2020-09-15 20:18:46 -0500

    First Tutorial Hello World

---
+++ pyTutorial1/start.py
@@ -0,0 +1,10 @@
+# Hello Guys! This is a Python Tutorial GitHub Project.
+# I will be pushing code to this repo from basics of Python
+# This is a open source project
+# This is latest version of Python tutorial 3.8.5
+# Author: Bibek Gupta
+
+# So, in Python, we need to type print and and any instructions between the quotation mark to print the command
+# on the screen
+
+print("Hello World")
