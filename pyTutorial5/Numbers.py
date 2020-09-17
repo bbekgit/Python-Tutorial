c9748046cfccf2cad7c85b6c11cadb5fcf459beb
Bibek Gupta <35547667+bbekgit@users.noreply.github.com>
2020-09-16 21:43:33 -0500

    Numbers
    Numbers function: addition, multiplication, power, square root, ceil, max, min, mod and so on

---
+++ pyTutorial5/Numbers.py
@@ -0,0 +1,86 @@
+# Here we are gonna work with numbers
+# python math import function
+from math import *
+
+# print a whole number
+print(2)
+
+# print a decimal number
+
+print(2.3456)
+
+# print a negative number
+
+print(-2.3456)
+
+# python addition
+
+print(5 + 6)
+
+# python decimal addition
+
+print(5 + 6.8)
+
+# python multiplication
+
+print(5 * 6.9)
+
+# python simplification
+
+print(4 * (6 + 9))
+
+# python mod function
+
+print(20 % 3)
+
+# store
+
+my_numb = 3
+print(my_numb)
+
+# convert into string
+my_numb = 3
+print(str(my_numb))
+
+my_numb = 7
+print(str(my_numb) + " is my chosen number")
+
+# print absolute number
+
+my_numb = -3
+print(abs(my_numb))
+
+# power function
+
+my_numb = -7
+print(pow(4, 2))
+
+# python max function
+
+print(max(7, 9))
+
+# python min function
+
+print(min(7, 9))
+
+# python round up function
+
+print(round(8.7))
+
+# python floor function
+
+print(floor(5.8))
+
+# python ceil function
+
+print(ceil(4.6))
+
+# python square root function
+
+print(sqrt(27))
+
+
+
+
+
+
