7aeba04a0f5bbb59a731556574656a5c36e3814b
Bibek Gupta <35547667+bbekgit@users.noreply.github.com>
2020-09-16 21:05:28 -0500

    Variable and Data Types

---
+++ pyTutorial3/variable_data.py
@@ -0,0 +1,8 @@
+# Variable and Data Types
+
+character_name = "Mary"
+character_age = "21"
+print("There was once a girl named " + character_name + ",")
+print("She was " + character_age + "years old.")
+print("She really liked her name which was given by her mom: " + character_name + ",")
+print("also liked being " + character_age + ".")
