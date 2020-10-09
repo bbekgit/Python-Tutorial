9ba4b574af0d1f0782484dc4e3b4c3c86d632323
Bibek Gupta <35547667+bbekgit@users.noreply.github.com>
2020-10-08 20:45:48 -0500

    Lists

---
+++ pyTutorial6/Lists.py
@@ -0,0 +1,24 @@
+# here we will learn about python lists
+
+lists = [2, 6, 9, 12, 24]
+
+# indexing returns the item
+
+print(lists[0])
+
+print(lists[-1])
+
+# slicing returns a new list
+
+print(lists[-3:])
+
+print(lists[:])
+
+print(lists + [0])
+
+print(lists + [36, 49, 64])
+
+
+
+
+
